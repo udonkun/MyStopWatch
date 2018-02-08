c3ae5f013a265771806c66d45b2df88f14b7f051
banseis <banseis75@gmail.com>
2018-02-08 23:07:13 +0900

    Add StopWatch Program

---
+++ app/src/main/java/edu/self/mystopwatch/MainActivity.kt
@@ -0,0 +1,63 @@
+package edu.self.mystopwatch
+
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val handler = Handler()
+    private var timeValue = 0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val runnable = object : Runnable {
+            override fun run() {
+                timeValue++
+                timeToText(timeValue)?.let {
+                    timeText.text = it
+                }
+                handler.postDelayed(this, 1000)
+            }
+        }
+
+        startButton.setOnClickListener {
+            if (timeValue==0){
+                handler.post(runnable)
+            }
+        }
+        stopButton.setOnClickListener {
+            handler.removeCallbacks(runnable)
+        }
+        resetButton.setOnClickListener {
+            handler.removeCallbacks(runnable)
+            timeValue = 0
+            timeToText()?.let {
+                timeText.text = it
+            }
+        }
+
+    }
+
+    private fun timeToText(time: Int = 0):String? {
+        return if (time < 0) {
+            null
+        }else if (time == 0) {
+            "00:00:00"
+        }else {
+            val h = time / 3600
+            val m = time % 3600 / 60
+            val s = time % 60
+            "%1$02d:%2$02d:%3$02d".format(h, m , s)
+        }
+    }
+}
+
